dc870c2a48aa5413e67d282aaa25ef1aba303161
JavaFXpert <james@j1w.xyz>
2016-08-12 14:26:26 -0400

    Retrieving documents from Devoxx corpus

--- src/main/java/watson/CallCorpus.java
+++ src/main/java/watson/CallCorpus.java
@@ -6,8 +6,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -17,12 +19,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import com.ibm.watson.developer_cloud.conversation.v1_experimental.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1_experimental.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1_experimental.model.MessageResponse;
 import com.ibm.watson.developer_cloud.service.exception.UnauthorizedException;
 import com.ibm.watson.developer_cloud.util.GsonSingleton;
+import watson.model.DevoxxPayload;
 
 /**
  * Created by jamesweaver on 8/11/16.
@@ -43,11 +50,27 @@ import com.ibm.watson.developer_cloud.util.GsonSingleton;
 
 
   public class CallCorpus {
+    private static final String SEARCH_REST_ENDPOINT = "http://sherlock.devoxx.com/api/search/";
     private static String API_VERSION = "v1-experimental";
     private static String PASSWORD = "Y8gUTfOHTB2b";
     private static String URL = "https://gateway.watsonplatform.net/conversation-experimental/api";
     private static String USERNAME = "371ffd54-0cab-49bd-b555-7b9320234ada";
 
+    private static final int TIMEOUT_IN_MILLIS = 15000;
+    private static final String OUTPUT_MODE = "outputMode";
+    private static final String JSON = "json";
+
+  /*
+  "credentials": {
+    "url": "https://gateway.watsonplatform.net/concept-insights/api",
+    "password": "FPhRNXvLnJ9m",
+    "username": "a425694b-f06a-4956-886d-e2e9e66d7c65"
+   */
+
+    private static final String INSIGHTS_USERNAME = "a425694b-f06a-4956-886d-e2e9e66d7c65";
+    private static final String INSIGHTS_PASSWORD = "FPhRNXvLnJ9m";
+
+    private static final Logger LOGGER = Logger.getLogger(CallCorpus.class.getName());
     private static boolean LOGGING_ENABLED = Boolean.parseBoolean(System.getenv("LOGGING_ENABLED"));
 
     private MessageRequest buildMessageFromPayload(InputStream body) {
@@ -106,9 +129,11 @@ import com.ibm.watson.developer_cloud.util.GsonSingleton;
 
       // Determine if conversation's response is sufficient to answer the user's question or if we
       // should call the retrieve and rank service to obtain better answers
-      if (response.getContext().containsKey("callRetrieveAndRank")) {
+      if (response.getOutput().toString().contains("callRetrieveAndRank")) {
         String query = response.getInputText();
         System.out.println("query: " + query);
+
+        retrieveDocument();
       }
       else {
         String query = response.getInputText();
@@ -140,5 +165,115 @@ import com.ibm.watson.developer_cloud.util.GsonSingleton;
     return retVal;
   }
 
+  private void retrieveDocument() throws IOException {
+    final Document doc = Jsoup.connect(SEARCH_REST_ENDPOINT + "lambda")
+        .timeout(TIMEOUT_IN_MILLIS)
+        .method(Connection.Method.GET)
+        .data("username", INSIGHTS_USERNAME)
+        .data("password", INSIGHTS_PASSWORD)
+        .data(OUTPUT_MODE, JSON)
+        .ignoreContentType(true)
+        .execute()
+        .parse();
 
+    LOGGER.info(doc.text());
+
+    final JsonElement jsonElement = new JsonParser().parse(doc.text());
+    final JsonArray jsonArray = jsonElement.getAsJsonArray();
+    Iterator iterator = jsonArray.iterator();
+    while (iterator.hasNext()) {
+      JsonObject jsonObject = (JsonObject)iterator.next();
+
+      DevoxxPayload devoxxPayload = new DevoxxPayload();
+
+      // Get the id
+      if (jsonObject.has("id")) {
+        devoxxPayload.setId(jsonObject.get("id").toString());
+        System.out.println("id: " + devoxxPayload.getId());
+      }
+
+      // Get the label
+      if (jsonObject.has("label")) {
+        devoxxPayload.setLabel(jsonObject.get("label").toString());
+        System.out.println("label: " + devoxxPayload.getLabel());
+      }
+
+      // Get the score
+      if (jsonObject.has("score")) {
+        devoxxPayload.setScore(jsonObject.get("score").toString());
+        System.out.println("score: " + devoxxPayload.getScore());
+      }
+
+      // Get the elements in userFields
+      if (jsonObject.has("userFields")) {
+        JsonObject userFields = (JsonObject) jsonObject.get("userFields");
+
+        // Get the authors
+        if (userFields.has("authors")) {
+          devoxxPayload.setAuthors(userFields.get("authors").toString());
+          System.out.println("authors: " + devoxxPayload.getAuthors());
+        }
+
+        // Get the emotions
+        if (userFields.has("emotions")) {
+          devoxxPayload.setEmotions(userFields.get("emotions").toString());
+          System.out.println("emotions: " + devoxxPayload.getEmotions());
+        }
+
+        // Get the language
+        if (userFields.has("language")) {
+          devoxxPayload.setLanguage(userFields.get("language").toString());
+          System.out.println("language: " + devoxxPayload.getLanguage());
+        }
+
+        // Get the link
+        if (userFields.has("link")) {
+          devoxxPayload.setLink(userFields.get("link").toString());
+          System.out.println("link: " + devoxxPayload.getLink());
+        }
+
+        // Get the publicationDate
+        if (userFields.has("publicationDate")) {
+          devoxxPayload.setPublicationDate(userFields.get("publicationDate").toString());
+          System.out.println("publicationDate: " + devoxxPayload.getPublicationDate());
+        }
+
+        // Get the sentiment
+        if (userFields.has("sentiment")) {
+          devoxxPayload.setSentiment(userFields.get("sentiment").toString());
+          System.out.println("sentiment: " + devoxxPayload.getSentiment());
+        }
+
+        // Get the thumbnail
+        if (userFields.has("thumbnail")) {
+          devoxxPayload.setThumbnail(userFields.get("thumbnail").toString());
+          System.out.println("thumbnail: " + devoxxPayload.getThumbnail());
+        }
+
+        // Get the thumbnailKeywords
+        if (userFields.has("thumbnailKeywords")) {
+          devoxxPayload.setThumbnailKeywords(userFields.get("thumbnailKeywords").toString());
+          System.out.println("thumbnailKeywords: " + devoxxPayload.getThumbnailKeywords());
+        }
+
+        //JsonElement authors = userFields.get("authors");
+        //System.out.println("authors: " + devoxxPayload.getAuthors());
+      }
+
+
+    }
+
+    /*
+    if (json.has(IMAGE_KEYWORDS)) {
+      final JsonArray imageKeywords = json.get(IMAGE_KEYWORDS).getAsJsonArray();
+
+      if (imageKeywords.size() > 0) {
+        return imageKeywords.get(0).getAsJsonObject().get("text").getAsString();
+      }
+    }
+    */
+
+    //return "no results";
+
+  }
 }
---
+++ src/main/java/watson/model/DevoxxPayload.java
@@ -0,0 +1,140 @@
+package watson.model;
+
+/**
+ * Created by jamesweaver on 8/12/16.
+ */
+public class DevoxxPayload {
+  private String id;
+  private String label;
+  private String score;
+  private String authors;
+  private String emotions;
+  private String language;
+  private String link;
+  private String publicationDate;
+  private String sentiment;
+  private String thumbnail;
+  private String thumbnailKeywords;
+
+  public DevoxxPayload() {
+  }
+
+  public DevoxxPayload(String id, String label, String score, String authors, String emotions, String language, String link, String publicationDate, String sentiment, String thumbnail, String thumbnailKeywords) {
+    this.id = id;
+    this.label = label;
+    this.score = score;
+    this.authors = authors;
+    this.emotions = emotions;
+    this.language = language;
+    this.link = link;
+    this.publicationDate = publicationDate;
+    this.sentiment = sentiment;
+    this.thumbnail = thumbnail;
+    this.thumbnailKeywords = thumbnailKeywords;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public String getScore() {
+    return score;
+  }
+
+  public void setScore(String score) {
+    this.score = score;
+  }
+
+  public String getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(String authors) {
+    this.authors = authors;
+  }
+
+  public String getEmotions() {
+    return emotions;
+  }
+
+  public void setEmotions(String emotions) {
+    this.emotions = emotions;
+  }
+
+  public String getLanguage() {
+    return language;
+  }
+
+  public void setLanguage(String language) {
+    this.language = language;
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+  public String getPublicationDate() {
+    return publicationDate;
+  }
+
+  public void setPublicationDate(String publicationDate) {
+    this.publicationDate = publicationDate;
+  }
+
+  public String getSentiment() {
+    return sentiment;
+  }
+
+  public void setSentiment(String sentiment) {
+    this.sentiment = sentiment;
+  }
+
+  public String getThumbnail() {
+    return thumbnail;
+  }
+
+  public void setThumbnail(String thumbnail) {
+    this.thumbnail = thumbnail;
+  }
+
+  public String getThumbnailKeywords() {
+    return thumbnailKeywords;
+  }
+
+  public void setThumbnailKeywords(String thumbnailKeywords) {
+    this.thumbnailKeywords = thumbnailKeywords;
+  }
+
+  @Override
+  public String toString() {
+    return "DevoxxPayload{" +
+        "id='" + id + '\'' +
+        ", label='" + label + '\'' +
+        ", score='" + score + '\'' +
+        ", authors='" + authors + '\'' +
+        ", emotions='" + emotions + '\'' +
+        ", language='" + language + '\'' +
+        ", link='" + link + '\'' +
+        ", publicationDate='" + publicationDate + '\'' +
+        ", sentiment='" + sentiment + '\'' +
+        ", thumbnail='" + thumbnail + '\'' +
+        ", thumbnailKeywords='" + thumbnailKeywords + '\'' +
+        '}';
+  }
+}
